145dfe8888c618170566a4f933dcec92092194d8
RyoOkumura <o9mura2000z@gmail.com>
2023-08-26 20:42:56 +0900

    style: Update styles and remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 import Loading from "./Loading";
-import Chat from "@/components/ui/Chat";
+import Chat from "@/components/Chat";
 import { getUser } from "@/lib/clerk";
-import Languages from "@/components/ui/Languages";
+import Languages from "@/components/Languages";
+import Examples from "@/components/Examples";
 
 export default async function Home() {
   const user = await getUser();
@@ -20,7 +20,7 @@ export default async function Home() {
               href="/account"
               className="text-purple-500 hover:border-b-2 hover:border-b-purple-500 "
             >
-              {user.firstName}
+              {user.username}
             </Link>
           ) : (
             <span className="text-orange-400">ゲスト</span>
@@ -28,26 +28,13 @@ export default async function Home() {
         </p>
       </div>
 
-      {!user && (
-        <div className="my-6">
-          <Link
-            href="/sign-in"
-            className="rounded bg-blue-500 p-3 text-white hover:bg-blue-400"
-          >
-            サインイン
-          </Link>
-        </div>
-      )}
-
       <Chat />
-      <div className="my-20">
+      <div className="my-4">
         <Suspense fallback={<Loading />}>
-          <a id="rooms" className="mx-2 text-gray-600">
-            Languages
-          </a>
           {/* @ts-expect-error Server Component */}
           <Languages />
         </Suspense>
+        <Examples></Examples>
       </div>
     </div>
   );
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="border-b border-gray-200">
-      <nav className="flex justify-between p-2">
-        <div className="flex">
+      <nav className="flex justify-between p-2 max-w-3xl mx-auto w-full">
+        <div className="flex ">
           <Link href={"/"} className="p-2 text-sm font-semibold md:text-2xl">
             Nanteyu
           </Link>
