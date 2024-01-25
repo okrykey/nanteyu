53ed564e9288c4bab25179ffc45804bd7e7c6a4c
okrykey <o9mura2000z@gmail.com>
2024-01-25 23:57:33 +0900

    refactor: Remove unnecessary part

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { jaJP } from "@clerk/localizations";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/layout/Header";
 import { Metadata } from "next";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nanteyu",
@@ -16,7 +13,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider localization={jaJP}>
       <html lang="ja">
-        <body className={inter.className}>
+        <body>
           <Header />
           <main className="mx-auto flex min-h-screen max-w-3xl flex-col place-content-center justify-between md:p-12">
             {children}
